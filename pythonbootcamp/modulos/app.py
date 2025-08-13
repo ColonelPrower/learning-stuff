0263ca10ba37370d46ad50c56d48d87884a1e988
ColonelPrower <alex.zurcat@gmail.com>
2025-08-13 14:00:32 -0600

    intro modulos y paquetes

---
+++ pythonbootcamp/modulos/app.py
@@ -0,0 +1,13 @@
+from usuarios.acciones.utilidades import guardar
+#from usuarios import acciones
+#import usuarios.acciones
+
+#best prac module palabra_palabra2_palabran
+guardar()
+print(__name__)
+#print(dir(usuarios)) #Lista paquetes
+#print(usuarios.__name__)
+#acciones.pagar_impuestos()
+
+if __name__ == "__main__":
+    print("tarea de mantenimiento si se ejecuta al inicio")
